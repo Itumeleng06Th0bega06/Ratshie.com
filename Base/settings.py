baaad4581ea7b257576cdd428d0739b5ece54cfe
Itumeleng Thobega <info@ratshie.com>
2025-09-02 10:09:03 +0200

    paypal stuff2

--- Base/settings.py
+++ Base/settings.py
@@ -16,7 +16,7 @@ load_dotenv()
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
+DEBUG = True
 
 
 #ALLOWED_HOSTS = []
