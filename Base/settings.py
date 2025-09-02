ea234b00aa2f570d552d0815f2031e4324acba0a
Itumeleng Thobega <info@ratshie.com>
2025-09-02 11:16:18 +0200

    paypal stuff2

--- Base/settings.py
+++ Base/settings.py
@@ -1,12 +1,12 @@
 from pathlib import Path
 import os
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # load varialbles
-load_dotenv()
+#load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
